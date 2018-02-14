688d78b67bd1782a883e2fae74cbe84641767457
Eddie Moon <eddiemoonie@gmail.com>
2018-02-14 00:35:11 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,8 @@ def oxford_comma(array)
   if array.length < 3
     array.join(" and " )
   else
-    last = array[-1]
+    last_element = array[-1]
     array.pop
-    array.join(" , ") << " and " << last
+    array.join(", ") << " and " << last_element
   end
 end
